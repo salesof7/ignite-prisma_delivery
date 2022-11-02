349e324e952309d32f4006cd85c8779669394544
salesof7 <jpsaless2002@gmail.com>
2022-11-02 11:18:23 -0300

    feat: ensure the creation of clients in the app with the encrypted password

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+import express, { json } from "express";
+import { routes } from "./routes";
+const app = express();
+
+app.use(json());
+
+app.use(routes);
+
+app.listen(3000, () => console.log("Server is running!"));
