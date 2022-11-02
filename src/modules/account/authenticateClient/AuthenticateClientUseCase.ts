4c19402d0c3bce36e462a55b9bce03f696106d4d
salesof7 <jpsaless2002@gmail.com>
2022-11-02 11:45:55 -0300

    feat: ensure authentication for clients by JWT token

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../../database/prismaClient";
+
+interface IAuthenticateClient {
+  username: string;
+  password: string;
+}
+
+class AuthenticateClientUseCase {
+  async execute({ username, password }: IAuthenticateClient): Promise<String> {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!client) {
+      throw new Error("Username or password invalid!");
+    }
+
+    const passwordMatch = await compare(password, client.password);
+
+    if (!passwordMatch) {
+      throw new Error("Username or password invalid!");
+    }
+
+    const token = sign({ username }, "251f2638ef112921bdc0a98609f871c3", {
+      subject: client.id,
+      expiresIn: "1d",
+    });
+
+    return token;
+  }
+}
+
+export { AuthenticateClientUseCase };
--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ app.use(json());
 
 app.use(routes);
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3333, () => console.log("Server is running!"));
